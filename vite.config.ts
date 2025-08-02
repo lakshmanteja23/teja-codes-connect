94045b6218b7ca97544c16e59f7f4d629d35b25e
Lakshman Teja <42748186+lakshmanteja23@users.noreply.github.com>
2025-08-02 13:32:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/  
 export default defineConfig(({ mode }) => ({
-  base: '/teja-codes-connect/'
+  // Add the base path here
+  base: '/teja-codes-connect/',
+  
   server: {
     host: "::",
     port: 8080,
